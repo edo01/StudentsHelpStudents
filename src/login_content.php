239c0bf3b3b7e5d8b787c60ecb895aa8ca31a817
edo01 <edocarra012@gmail.com>
2020-04-28 20:41:18 +0200

    Implement Ajax

---
+++ src/login_content.php
@@ -0,0 +1,9 @@
+<h4>username:</h4>
+<input type="text" name="usernamel" required>
+<br>
+<h4>password:</h4>
+<input type="password" name="passwordl" required>
+<br>
+<input type="submit" name="submit" value="login"><h3> oppure </h3> <a onclick="loadSignup()">registrati</a>            
+
+
---
+++ src/signup_content.php
@@ -0,0 +1,30 @@
+<!-- TO DO: 
+    - check the email
+    - send an email to confirm 
+    - check the class -->
+
+<h4>username:</h4>
+<input type="text" name="username" required>
+<br>
+<h4>password:</h4>
+<input type="password" name="password" required>
+<br>
+<h4>ripeti password:</h4>
+<input type="password" name="password2" required>
+<br>
+<h4>nome</h4>
+<input type="text" name="name" required>
+
+<h4>cognome</h4>
+<input type="text" name="surname" required>
+<br>
+
+<h4>class</h4>
+<input type="text" name="class" required>
+<br>
+
+<br>
+<h4>email istituzionale:</h4>
+<input type="text" name="email" required>
+<br>
+<input type="submit" name="submit" value="registrati"> oppure <a onclick="loadLogin()">accedi</a>  
